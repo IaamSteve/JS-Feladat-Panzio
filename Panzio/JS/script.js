e022036c21279bcc11b6f727c3019871e0d05f49
KIstvan <kerekesistvan@turr.hu>
2022-09-19 12:30:16 +0200

    Otthon végzet feladat

---
+++ Panzio/JS/script.js
@@ -0,0 +1,27 @@
+let vendegek = document.querySelector('#vendegek'),
+    sDate = document.querySelector('#start'),
+    eDate = document.querySelector('#stop'),
+    szoba1 = document.querySelector('#Szoba1'),
+    szoba2 = document.querySelector('#Szoba2'),
+    szoba3 = document.querySelector('#Szoba3'),
+    szoba4 = document.querySelector('#Szoba4'),
+    furdo1 = document.querySelector('#Furdo1'),
+    furdo2 = document.querySelector('#Furdo2'),
+    furdo3 = document.querySelector('#Furdo3'),
+    furdo4 = document.querySelector('#Furdo4'),
+    egyeb = document.querySelector('#Egyeb');
+
+/*
+var d1 = new Date(); //az aktuális dátum
+var d2 = new Date("2013/11/01"); //a múltbéli dátum
+var diff = Math.abs(d1 - d2); //a különbség(ms)
+document.write(Math.round(diff / 1000 / 60 / 60 / 24 / 31)); //az eredmény
+
+function szokoev(ev) {
+    let nap = 0;
+    if ((ev % 4 == 0) && (ev % 100 != 0) || (ev % 400 == 0)) {
+        nap = 1;
+    }
+    return nap;
+}
+*/
