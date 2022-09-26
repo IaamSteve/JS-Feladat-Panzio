bf17c847ab2bf8ecef1d271b2d4371195b2e9f35
KIstvan <kerekesistvan@turr.hu>
2022-09-26 15:46:51 +0200

    JQuery initialized

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,38 @@
-let vendegek = document.querySelector('#vendegek'),
-    sDate = document.querySelector('#start'),
-    eDate = document.querySelector('#stop'),
-    szoba1 = document.querySelector('#Szoba1'),
-    szoba2 = document.querySelector('#Szoba2'),
-    szoba3 = document.querySelector('#Szoba3'),
-    szoba4 = document.querySelector('#Szoba4'),
-    furdo1 = document.querySelector('#Furdo1'),
-    furdo2 = document.querySelector('#Furdo2'),
-    furdo3 = document.querySelector('#Furdo3'),
-    furdo4 = document.querySelector('#Furdo4'),
-    egyeb = document.querySelector('#Egyeb');
+let perfo = $('#vendegek'),
+    sDate = $('#start'),
+    eDate = $('#stop'),
+    szoba1 = $('#Szoba1'),
+    szoba2 = $('#Szoba2'),
+    szoba3 = $('#Szoba3'),
+    szoba4 = $('#Szoba4'),
+    furdo1 = $('#Furdo1'),
+    furdo2 = $('#Furdo2'),
+    furdo3 = $('#Furdo3'),
+    furdo4 = $('#Furdo4'),
+    egyeb = $('#Egyeb');
+/*
+var d1 = Date.getTime(),
+    d2 = 
+    */
+$(function() {
+    $("#sDate").datepicker();
+    var Enter = $("#sDate")
+});
+$(function() {
+    $("#eDate").datepicker();
+    var leave = $("#eDate")
+});
 
+function func() {
+    sDate = new Date(date1.value);
+    eDate = new Date(date2.value);
+    var milli_secs = sDate.getTime() - eDate.getTime();
+
+    // Convert the milli seconds to Days 
+    var days = milli_secs / (1000 * 3600 * 24);
+    document.getElementById("difference").innerHTML =
+        Math.round(Math.abs(days));
+}
 /*
 var d1 = new Date(); //az aktuális dátum
 var d2 = new Date("2013/11/01"); //a múltbéli dátum
