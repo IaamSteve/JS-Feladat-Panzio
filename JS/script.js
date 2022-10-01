5611329b7f45784be9ccad25eeb1d8387b7996a1
KIstvan <kerekesistvan@turr.hu>
2022-10-01 16:40:30 +0200

    k

--- JS/script.js
+++ JS/script.js
@@ -2,42 +2,46 @@
 import './flatpickr'
 import $ from 'jquery'
 */
-
-
-$(".selector").flatpickr(optional_config)
+/*
 let perfo = $('#vendegek'),
     sDate = $('#start'),
     eDate = $('#stop'),
     szoba1 = $('#Szoba1'),
     szoba2 = $('#Szoba2'),
-    // szoba3 = $('#Szoba3'),
+    szoba3 = $('#Szoba3'),
     szoba4 = $('#Szoba4'),
     furdo1 = $('#Furdo1'),
     furdo2 = $('#Furdo2'),
     furdo3 = $('#Furdo3'),
     furdo4 = $('#Furdo4'),
     egyeb = $('#Egyeb');
-/*
-var d1 = Date.getTime(),
-    d2 = 
-    */
-$(function() {
-    $('form').submit(function(event) {
-        let date1 = JSON.stringify($(this).serializeArray())
-        $('#message').text(date1)
-        event.preventDefault()
-    })
-})
-
-function func() {
-    sDate = new Date(date1.value)
-    eDate = new Date(date2.value)
-    var milli_secs = sDate.getTime() - eDate.getTime()
-
-    // Convert the milli seconds to Days 
-    var days = Math.round(Math.abs(milli_secs / (1000 * 3600 * 24)))
-    console.log(days)
+*/ //Failed Jquery
+let d1;
+let d2;
+document.getElementById("date1").addEventListener("change", getDate);
+document.getElementById("date2").addEventListener("change", getDate1);
+function getDate() {
+  d1 = document.getElementById("date1");
+  console.log(d1.value);
 }
+function getDate1() {
+  d2 = document.getElementById("date2");
+  console.log(d2.value);
+}
+function DiffOfTime(){
+    const diffTime = Math.abs(d2.value - d1.value);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    console.log(diffTime + " milliseconds");
+    console.log(diffDays + " days");
+    
+}
+
+/*
+function dateDiff(d1, d2) {
+  var datediff = d1.value - d2.value; //store the getTime diff - or +
+  return datediff / (24 * 60 * 60 * 1000); //Convert values to -/+ days and return value
+}
+
 /*
 var d1 = new Date(); //az aktuális dátum
 var d2 = new Date("2013/11/01"); //a múltbéli dátum
